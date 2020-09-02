80431e90b4559fd4ffaca00f434e60f3d8043026
Pratik Ghimire <63243723+prateekghimire@users.noreply.github.com>
2020-09-02 17:10:11 +0545

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template,request
+from pytube import YouTube
+
+
+
+app=Flask(__name__)
+
+
+@app.route('/')
+
+def home():
+    link="https://www.youtube.com/watch?v=SmTDwsomYno"
+
+    yt=YouTube(link)
+
+    print(yt.title)
+
+    print(yt.rating)
+
+    ys=yt.streams.get_highest_resolution()
+
+    print("*****Downloading******")
+    ys.download()
+
+    return 'Youtube downloaded'
+
+app.run(debug=True,port=0000)
+    
